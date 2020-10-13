ca9d177be7cdf2f894df3d3378f24a27a8727385
ChenYizhu97 <cyzhaa@gmail.com>
2020-10-13 04:13:07 +0200

    implementing blocked matrix multiplication

--- project1/matmult/dgemm-blocked.c
+++ project1/matmult/dgemm-blocked.c
@@ -12,8 +12,8 @@ MKLROOT = /opt/intel/composer_xe_2013.1.117/mkl
 LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKLROOT)/lib/intel64/libmkl_sequential.a $(MKLROOT)/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm
 
 */
-
-const char* dgemm_desc = "Naive, three-loop dgemm.";
+#include <math.h>
+const char* dgemm_desc = "blocked dgemm.";
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
@@ -23,15 +23,27 @@ void square_dgemm (int n, double* A, double* B, double* C)
 {
   // TODO: Implement the blocking optimization
 
-  /* For each row i of A */
-  for (int i = 0; i < n; ++i)
-    /* For each column j of B */
-    for (int j = 0; j < n; ++j) 
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*n];
-      for( int k = 0; k < n; k++ )
-	cij += A[i+k*n] * B[k+j*n];
-      C[i+j*n] = cij;
+  int BLOCK_SIZE = 16;
+
+
+  /* For each row i of Blocked C */
+  for (int ii = 0; ii < n; ii += BLOCK_SIZE)
+    /* For each column j of Blocked C */
+    for (int jj = 0; jj < n; jj += BLOCK_SIZE) 
+    {  
+      /* Compute block C(i,j) */
+      for (int kk = 0; kk < n; kk += BLOCK_SIZE)     
+      {
+        for (int i = ii; i < (ii+BLOCK_SIZE < n ? ii+BLOCK_SIZE:n); ++i)
+        { 
+          for (int  j= jj; j < (jj+BLOCK_SIZE < n ? jj+BLOCK_SIZE:n); ++j)
+          {
+            double cij = 0;
+            for (int  k = kk; k < (kk+BLOCK_SIZE < n ? kk+BLOCK_SIZE:n); ++k)
+              cij += A[i+k*n]*B[k+j*n];
+            C[i+j*n] += cij;
+          }
+        }
+      }
     }
 }
